db5c567b4b77481029b2bb44b3bbe1e4d6136291
Robgotherearly <robbmarsh93@gmail.com>
2026-04-24 15:20:52 +0000

    Add Flask web server

--- app.py
+++ app.py
@@ -1,7 +1,20 @@
-# app.py
+from flask import Flask
+
+app = Flask(__name__)
+
+
 def add(a, b):
     return a + b
 
 
 def subtract(a, b):
     return a - b
+
+
+@app.route('/')
+def home():
+    return f'Result: {add(2, 3)}'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8000)
