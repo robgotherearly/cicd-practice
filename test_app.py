c47fb57a0b724b5a53ef310be031228ef6d1d418
Robgotherearly <robbmarsh93@gmail.com>
2026-04-24 09:47:37 +0000

    Fix flake8 linting errors

--- test_app.py
+++ test_app.py
@@ -1,8 +1,9 @@
-# test_app.py
 from app import add, subtract
 
+
 def test_add():
-    assert add(2, 3) == 5  # fixed!
+    assert add(2, 3) == 5
+
 
 def test_subtract():
     assert subtract(5, 3) == 2
