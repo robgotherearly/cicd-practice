e0a79f27e3e5908e7b9c398ba3e8ffc4aba63d9c
Robgotherearly <robbmarsh93@gmail.com>
2026-04-24 09:12:26 +0000

    Fix broken test

--- test_app.py
+++ test_app.py
@@ -2,7 +2,7 @@
 from app import add, subtract
 
 def test_add():
-    assert add(2, 3) == 99  # intentionally wrong!
+    assert add(2, 3) == 5  # fixed!
 
 def test_subtract():
     assert subtract(5, 3) == 2
