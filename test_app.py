d0f4551d5321b07f435774b602a518511b3fc187
Robgotherearly <robbmarsh93@gmail.com>
2026-04-24 09:09:30 +0000

    Intentionally broken test

--- test_app.py
+++ test_app.py
@@ -2,7 +2,7 @@
 from app import add, subtract
 
 def test_add():
-    assert add(2, 3) == 5
+    assert add(2, 3) == 99  # intentionally wrong!
 
 def test_subtract():
     assert subtract(5, 3) == 2
